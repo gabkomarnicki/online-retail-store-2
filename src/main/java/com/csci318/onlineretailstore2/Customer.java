9a0e289a6fe2bf3a5f97f7072413d617fa00c7b4
Gabriel Komarnicki <20436870+gabkomarnicki@users.noreply.github.com>
2021-08-12 21:17:07 +1000

    Created Customer.java

---
+++ src/main/java/com/csci318/onlineretailstore2/Customer.java
@@ -0,0 +1,87 @@
+package com.csci318.onlineretailstore2;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String companyName;
+    private String address;
+    private String country;
+
+    public Customer() {
+    }
+
+    public Customer(String companyName, String address, String country) {
+        this.companyName = companyName;
+        this.address = address;
+        this.country = country;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", companyName='" + companyName + '\'' +
+                ", address='" + address + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o)
+            return true;
+        if (!(o instanceof Customer))
+            return false;
+        Customer Customer = (Customer) o;
+        return Objects.equals(this.id, Customer.id)
+                && Objects.equals(this.companyName, Customer.companyName)
+                && Objects.equals(this.address, Customer.address)
+                && Objects.equals(this.country, Customer.country);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id, this.companyName, this.address, this.country);
+    }
+}
